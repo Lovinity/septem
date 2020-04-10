4674147e670ef41bcd81b7cb6594a76773634b46
Patrick Schmalstig <xanaftp@gmail.com>
2020-04-10 19:20:55 -0400

    Use local.js for config

---
+++ config/custom.js
@@ -0,0 +1,24 @@
+/**
+ * Custom configuration
+ * (sails.config.custom)
+ *
+ * One-off settings specific to your application.
+ *
+ * For more information on custom configuration, visit:
+ * https://sailsjs.com/config/custom
+ */
+
+module.exports.custom = {
+  discord: {
+    clientOptions: { // Discord.js clientOptions. You can override in local.js
+      messageCacheMaxSize: 10000,
+      messageCacheLifetime: (60 * 60 * 24 * 10),
+      messageSweepInterval: (60 * 60),
+      fetchAllMembers: true,
+      partials: [ 'USER', 'MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'REACTION' ],
+    },
+    token: `` // Specify in local.js
+  },
+
+  baseURL: `https://discord.septem-regiones.fun`
+};
